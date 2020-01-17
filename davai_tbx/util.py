e05cb000b9338e6afa0435dc4b1e9fc1056d3451
Alexandre Mary <alexandre.mary@meteo.fr>
2020-01-17 14:55:56 +0100

    fix: morph_with_points

--- davai_tbx/util.py
+++ davai_tbx/util.py
@@ -50,23 +50,112 @@ class TaskSummary(dict):
         self.update(asdict)
 
 
-def write_xpinfo(user, xpid, ref_xpid, appenv, commonenv, input_store, usecase,
-                 appenv_details, commonenv_details):
+def write_xpinfo(user, xpid, ref_xpid, usecase, commonenv, commonenv_details,
+                 appenv=None, input_store=None, appenv_details=None,  # DEPRECATED
+                 appenv_lam=None, appenv_global=None, appenv_clim=None,
+                 appenv_lam_details=None, appenv_global_details=None,
+                 input_store_lam=None, input_store_global=None):
     """Write info about experiment in 'xpinfo.json'."""
-    metadata = {'initial_time_of_launch':date.utcnow().isoformat().split('.')[0], # + ' Z',
+    metadata = {'initial_time_of_launch':date.utcnow().isoformat().split('.')[0],
                 'user':user,
                 'xpid':xpid,
                 'ref_xpid':ref_xpid,
-                'appenv':appenv,
                 'commonenv':commonenv,
-                'input_store':input_store,
                 'usecase':usecase,
-                'appenv_details':'<br>'.join(appenv_details),
                 'commonenv_details':'<br>'.join(commonenv_details),
                 # optional
                 'git_branch':os.environ.get('GIT_BRANCH'),
                 'comment':os.environ.get('COMMENT'),
                 'pack':os.environ.get('PACK'),
                 }
+    # DEPRECATED
+    if appenv:
+        metadata['appenv'] = appenv
+    if input_store:
+        metadata['input_store'] = input_store
+    if appenv_details:
+        metadata['appenv_details'] = '<br>'.join(appenv_details)
+    # NEW
+    if appenv_lam:
+        metadata['appenv_lam'] = appenv_lam
+    if appenv_global:
+        metadata['appenv_global'] = appenv_global
+    if appenv_clim:
+        metadata['appenv_clim'] = appenv_clim
+    if input_store_lam:
+        metadata['input_store_lam'] = input_store_lam
+    if input_store_global:
+        metadata['input_store_global'] = input_store_global
+    if appenv_lam_details:
+        metadata['appenv_lam_details'] = '<br>'.join(appenv_lam_details)
+    if appenv_global_details:
+        metadata['appenv_global_details'] = '<br>'.join(appenv_global_details)
+    # write
     with open('xpinfo.json', 'w') as out:
         json.dump(metadata, out, indent=4, sort_keys=True)
+
+
+class XPMetadata(object):
+
+    env_catalog_variables = ('APPENV', 'APPENV_LAM', 'APPENV_GLOBAL',
+                             'APPENV_CLIM', 'COMMONENV')
+
+    def __init__(self, xpid):
+        self._dict = {'xpid':xpid,
+                      'initial_time_of_launch':date.utcnow().isoformat().split('.')[0],
+                      'user':os.environ['USER'],
+                      # absent-safe
+                      'ref_xpid':os.environ.get('REF_XPID'),
+                      'usecase':os.environ.get('USECASE'),
+                      'git_branch':os.environ.get('GIT_BRANCH'),
+                      'comment':os.environ.get('COMMENT'),
+                      'pack':os.environ.get('PACK'),
+                      }
+        for k in self.env_catalog_variables:
+            e = os.environ.get(k)
+            if e:
+                self.set(k.lower(), e)
+        for k in ('INPUT_STORE', 'INPUT_STORE_LAM', 'INPUT_STORE_GLOBAL'):
+            s = os.environ.get(k)
+            if s:
+                self.set(k.lower(), s)
+    
+    def set(self, k, v):
+        self._dict[k] = v
+    
+    def get(self, k=None):
+        if k is None:
+            return copy.copy(self._dict)
+        else:
+            return self._dict[k]
+    
+    def which_env_catalog_details(self):
+        return {k.lower():self._dict.get(k.lower())
+                for k in self.env_catalog_variables
+                if k.lower() in self._dict}
+    
+    def set_env_catalog_details(self, k, details):
+        self._dict['{}_details'.format(k.lower())] = '<br>'.join(details)
+    
+    def write(self):
+        with open('xpinfo.json', 'w') as out:
+            json.dump(metadata, out, indent=4, sort_keys=True)
+
+def XP_which_env_to_pull():
+    """Return a list of genv/uenv to be pulled."""
+    envs = {}
+    for k in :
+        e = os.environ.get(k)
+        if e:
+            envs[k] = e
+    return envs
+
+
+def XP_metadata_collect(metadata):
+    """Collect a series of metadata from environment variables."""
+    
+    for k, e in XP_which_env_to_pull().items():
+        metadata[k.lower()] = e
+    return metadata
+
+def XPinfo_write():
