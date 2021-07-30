8fc4e1d0f5ce7675fde4be01734cb0fa88e92684
MARY Alexandre <alexandre.mary@meteo.fr>
2021-07-30 14:02:16 +0000

    Label 1.1.1

--- src/ial_expertise/__init__.py
+++ src/ial_expertise/__init__.py
@@ -5,6 +5,6 @@ IAL expertise package: expertise outputs of IAL tasks.
 """
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 from . import experts, task
