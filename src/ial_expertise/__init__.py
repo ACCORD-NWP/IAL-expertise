dee4d952c04ade1367e09f1d8f90b7a215d94754
Alexandre Mary <alexandre.mary@meteo.fr>
2022-06-08 11:58:55 +0200

    VERSION taken out __init__.py

--- src/ial_expertise/__init__.py
+++ src/ial_expertise/__init__.py
@@ -5,6 +5,10 @@ IAL expertise package: expertise outputs of IAL tasks.
 """
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-__version__ = '1.1.2'
+import os
+import io
+
+package_rootdir = os.path.dirname(os.path.dirname(os.path.realpath(__path__[0])))  # realpath to resolve symlinks
+__version__ = io.open(os.path.join(package_rootdir, 'VERSION'), 'r').read().strip()
 
 from . import experts, task
