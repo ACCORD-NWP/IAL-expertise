55f86cb61c9df8d2b8b2ec120febd1ebf05f02fe
Daan Degrauwe <daan.degrauwe@meteo.be>
2025-11-07 10:50:46 +0000

    Added coding norms expert

--- src/ial_expertise/experts/build.py
+++ src/ial_expertise/experts/build.py
@@ -28,8 +28,9 @@ class GmkpackBuildExpert(OutputExpert):
     def _parse(self):
         with io.open(self.output, 'r') as f:
             self.parsedOut = json.load(f)
-
+        
     def summary(self):
+
         summary = {'All OK':all([v['OK'] for v in self.parsedOut.values()]),
                    'Executables OK':sorted([k for k, v in self.parsedOut.items() if v['OK']]),
                    'Executables failed':sorted([k for k, v in self.parsedOut.items() if not v['OK']]),
@@ -52,3 +53,89 @@ class GmkpackBuildExpert(OutputExpert):
                 'Validated':test_failed.issubset(ref_failed),
                 'Newly missing executables':len(test_failed.difference(ref_failed)),
                 'mainMetrics':'Newly missing executables'}
+
+class CodingNormsExpert(OutputExpert):
+    """" expert to check coding norms during IALGitRef2Pack """
+
+    _footprint = dict(
+        info = 'Check coding norms violations',
+        attr = dict(
+            kind = dict(
+                values = ['codingnorms',],
+            ),
+            output = dict(
+                info = "Coding norms for local files.",
+                optional = True,
+                default = 'coding_norms.json',
+            ),
+        )
+    )
+
+    def _parse(self):
+        with io.open(self.output, 'r') as f:
+            self.parsedInput = json.load(f)
+
+    def summary(self):
+
+        summary={}
+
+        if 'local' in self.parsedInput:
+
+            sourcefiles_improved=[]
+            sourcefiles_degraded=[]
+            sourcefiles_neutral=[]
+            
+            if 'main' in self.parsedInput:
+                # possible to make comparison
+
+                for sourcefile in self.parsedInput['local']:
+                    # total number of violations
+                    localcount=sum([self.parsedInput['local'][sourcefile][violatedNorm] for violatedNorm in self.parsedInput['local'][sourcefile]])
+                    if sourcefile in self.parsedInput['main']:
+                        maincount=sum([self.parsedInput['main'][sourcefile][violatedNorm] for violatedNorm in self.parsedInput['main'][sourcefile]])
+                    else:
+                        maincount=0
+                    # add file to appropriate list
+                    if localcount<maincount:
+                        sourcefiles_improved.append(sourcefile)
+                    elif localcount==maincount:
+                        sourcefiles_neutral.append(sourcefile)
+                    else:
+                        sourcefiles_degraded.append(sourcefile)
+                
+                # add lists of files to summary
+                summary['sourcefiles improved']=sourcefiles_improved
+                summary['sourcefiles degraded']=sourcefiles_degraded
+                summary['sourcefiles neutral']=sourcefiles_neutral
+
+                # add background info
+                summary['_main']=self.parsedInput['main']
+
+                # main validation metric
+                summary['Auto-test']='Failed' if len(sourcefiles_degraded)>0 else 'Passed'
+
+            # add background info
+            summary['_local']=self.parsedInput['local']
+
+        return summary
+    
+    def _compare(self, references, *args, **kwargs):
+        return self._compare_summaries(references, *args, **kwargs)
+
+    @classmethod
+    def compare_2summaries(cls, test, ref):
+        if '_local' in test and '_main' in test:
+            comparison={'Validated means':'No source files have degraded in terms of coding norms.',
+                'Validated':len(test['sourcefiles_degraded'])==0,
+                'Number of files degraded':len(test['sourcefiles_degraded']),
+                'mainMetrics':'Number of files degraded',
+                'Source files improved':test['sourcefiles_improved'],
+                'Source files degraded':test['sourcefiles_degraded'],
+                'Source files neutral':test['sourcefiles_neutral'],
+            }
+        else:
+          comparison={'Validated means':'Coding norms not checked; unable to validate',
+                'Validated':True,
+                'Number of files degraded':0,
+                'mainMetrics':'Number of files degraded',
+            }
